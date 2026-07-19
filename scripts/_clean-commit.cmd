5b6ed7fb5435c182e43da504a02bbd02c34cec6e
serve <imvu-companion@users.noreply.github.com>
2026-07-19 08:16:49 -0400

    chore: remove temporary clean-commit helper

--- scripts/_clean-commit.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d C:\Users\serve\ansel\IMVUCompanion
-dotnet build IMVUCompanion.csproj -c Release --nologo -v q > "%TEMP%\imvu-build.txt" 2>&1
-echo BUILD_EXIT=%ERRORLEVEL%>> "%TEMP%\imvu-build.txt"
-git add -A
-git status --short >> "%TEMP%\imvu-build.txt"
-git commit -m "chore: remove obsolete research dumps, one-off scripts, and old installers" >> "%TEMP%\imvu-build.txt" 2>&1
-git push origin HEAD >> "%TEMP%\imvu-build.txt" 2>&1
-echo DONE>> "%TEMP%\imvu-build.txt"
